a78d5c5875aae181e6fcd31cc42d7f8f1b2f2222
unknown <raveen.anand@whitehatjr.com>
2021-10-21 19:29:51 +0530

    v2

--- myscript.js
+++ myscript.js
@@ -1,27 +1,26 @@
 function validate()
 {
     //1. Take input from name and contact fields , store them in variables
-    var name = document.getElementById("userName").value;
-    var phone = document.getElementById("contactInput").value;
+    var name = document.getElementById("uname").value;
+    var phone = document.getElementById("contact").value;
 
-    //2. calculate length of contact number and store i a variable named "count"
+    //2. calculate length of contact number and store it in a variable named "count"
     var count= phone.length;
-    console.log(count);
     
     //3. if name input is blank, display the 'err1' element, else hide it
     if(name == ''){
-        document.getElementById('err1').style.display="block";
+        document.getElementById("err1").style.display="block";
     }
     else{
-        document.getElementById('err1').style.display="none";
+        document.getElementById("err1").style.display="none";
     }
 
-    //4. if Count is not equal to 10, display the 'err2' element, else hide it
+    //4. if count is not equal to 10, display the 'err2' element, else hide it*/
     if(count != 10){
-        document.getElementById('err2').style.display="block";
+        document.getElementById("err2").style.display="block";
     }
     else{
-        document.getElementById('err2').style.display="none";
+        document.getElementById("err2").style.display="none";
     }
     
 }
